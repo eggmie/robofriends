42fc4383694d0459ff4e75c9368358458de931a6
Eggmie <xyzhang.me@gmail.com>
2019-01-15 18:59:35 +0800

    Using logger and thunk.

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,54 +5,39 @@ import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
 
-import { setSearchField } from '../actions'
+import { setSearchField, requestRobots } from '../actions'
 
 const mapStateToProps = (state) => {
 	return {
-		searchField: state.searchField,
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error,
 	};
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots()),
 	};
 }
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			// searchfield: '',
-		};
-		console.log(1);
-	}
-
 	componentDidMount() {
-		console.log(2);
-		// console.log(this.props.store.getState());
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((res) => (res.json()))
-			.then((users) => (this.setState({robots: users})));
+		console.log('componentDidMount');
+		this.props.onRequestRobots();
 	}
-
-	// onSearchChange = (event) => {
-	// 	console.log(event.target.value);
-	// 	this.setState({searchfield: event.target.value});
-	// }
 
 	render() {
-		console.log(3);
-		// const filteredRobots = this.state.robots.filter((robot) => {
-		// 	return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		// });
-		const {robots} = this.state;
-		const {searchField, onSearchChange} = this.props;
+		console.log('render');
+
+		const {searchField, onSearchChange, robots, isPending} = this.props;
+		console.log(robots);
 		const filteredRobots = robots.filter((robot) => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
-		if (!robots.length) {
+		if (isPending) {
 			return (<h2 className='tc'>Loading</h2>);
 		} else {
 			return (
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,16 @@
-export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
+import { 
+	CHANGE_SEARCH_FIELD,
+	REQUEST_ROBOTS_PENDING,
+	REQUEST_ROBOTS_SUCCESS,
+	REQUEST_ROBOTS_FAILED,
+} from './constants.js'
 
-const intialState = {
+const intialStateSearch = {
 	searchField: ''
 }
 
-export const searchRobots = (state=intialState, action={}) => {
-	console.log('action.type', action.type);
+export const searchRobots = (state=intialStateSearch, action={}) => {
+	// console.log('action.type', action.type);
 	switch(action.type) {
 		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, {searchField: action.payload});
@@ -13,3 +18,22 @@ export const searchRobots = (state=intialState, action={}) => {
 			return state;
 	}
 };
+
+const intialStateRobots = {
+	isPending: false,
+	robots: [],
+	error: ''
+}
+
+export const requestRobots = (state=intialStateRobots, action={}) => {
+	switch(action.type) {
+		case REQUEST_ROBOTS_PENDING:
+			return Object.assign({}, state, {isPending: true});
+		case REQUEST_ROBOTS_SUCCESS:
+			return Object.assign({}, state, {robots: action.payload, isPending: false});
+		case REQUEST_ROBOTS_FAILED:
+			return Object.assign({}, state, {error: action.payload, isPending: false});
+		default:
+			return state;
+	}
+}
